d71e755bd864096b0da5ef79c046d486d1135c3f
Roshni Padam <padam.roshni13@gmail.com>
2019-03-14 11:28:21 +0530

    Update CustomConsumer.java

--- CustomConsumer.java
+++ CustomConsumer.java
@@ -1,13 +1,18 @@
 package com.random.kafka;
-
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.spark.SparkConf;
 import org.apache.spark.TaskContext;
 import org.apache.spark.api.java.*;
 import org.apache.spark.api.java.function.*;
+import org.apache.spark.sql.sources.In;
 import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.streaming.kafka010.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -17,7 +22,8 @@ import org.apache.spark.streaming.Durations;
 public class CustomConsumer {
 
     public static void main(String[] args) {
-         final Pattern SPACE = Pattern.compile(" ");
+        Pattern aa = Pattern.compile("(\\[|\\])");
+        Pattern mat = Pattern.compile("-?\\d+");
         Map<String, Object> kafkaParams = new HashMap<>();
         kafkaParams.put("bootstrap.servers", "localhost:9092");
         kafkaParams.put("key.deserializer", StringDeserializer.class);
@@ -28,20 +34,45 @@ public class CustomConsumer {
         SparkConf sparkConf = new SparkConf().setAppName("aaa").setMaster("local[2]");
 
         JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
-        Collection<String> topics = Arrays.asList("random-numbers");
+        Collection<String> topicName = Arrays.asList("fun");
 
-        JavaInputDStream<ConsumerRecord<String, String>> stream =
+        JavaInputDStream<ConsumerRecord<Integer, String>> stream =
                 KafkaUtils.createDirectStream(
                         jssc,
                         LocationStrategies.PreferConsistent(),
-                        ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
+                        ConsumerStrategies.<Integer, String>Subscribe(topicName, kafkaParams)
                 );
-
         //spark streaming actions
-        JavaDStream<String> lines = stream.map(ConsumerRecord::value);
-//        JavaDStream<String> words =lines.flatMap(x -> Arrays.asList(SPACE.split(x)).iterator());
+        JavaDStream<String> lines1 = stream.map(ConsumerRecord::value);
 
-        lines.print();
+        System.out.println("Printing DSTREAM");
+        lines1.print();
+
+       lines1.foreachRDD(rdd -> {
+            List<String> collect = rdd.collect();
+           System.out.println("Printing List of String : " + collect);
+           List<Integer> intNum = new ArrayList<>();
+           for (String numbers: collect) {
+               numbers = numbers.replaceAll("\\[", "").replaceAll("\\]","");
+               System.out.println("Number : " + numbers);
+               String[] split = numbers.split(",");
+               int sum = 0;
+               for (String num : split) {
+                   int i = Integer.parseInt(num.trim());
+                   sum = sum + i;
+               }
+               int avg = sum / split.length;
+               System.out.println("SUM : " + sum + " Lenght : " + split.length);
+               System.out.println("Average : " + avg
+               );
+           }
+
+        });
+
+
+
+    //
+
         jssc.start();
         try {
             jssc.awaitTermination();
